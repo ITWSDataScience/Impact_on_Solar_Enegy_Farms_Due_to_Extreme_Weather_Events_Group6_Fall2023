035c0f692eef95d34952c880eb02091c60238491
Tony Min <mint@rpi.edu>
2023-10-26 13:16:12 -0400

    Parsing Python Scripts
    Pair / Trio Programming

---
+++ parse_nri_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 12:33:38 2023
+
+@author: ghpan
+"""
+import pandas as pd 
+
+#def parse(gen_csv = False):
+df = pd.read_csv("NRI_Table_Counties_California.csv")
+    
+df = df[["COUNTY", "ALR_VALB", "EAL_VALB", "EAL_VALT", "RISK_VALUE", 
+         "RISK_SCORE", "RISK_SPCTL", "EAL_SPCTL", "BUILDVALUE", 
+         "AREA"]]
---
+++ parse_power_plant.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 12:05:49 2023
+
+@author: ghpan
+"""
+
+import pandas as pd 
+
+def parse(gen_csv = False):
+    df = pd.read_csv("California_Power_Plants.csv")
+    # Filter out Primary Energy Source that isn't sun
+    df = df[df["PriEnergySource"] == "SUN"]
+    # Filter Retired plants out
+    df = df[df["Retired_Plant"] == 0]
+    
+    df = df.drop(columns = ["Retired_Plant", "OperatorCompanyID", "PriEnergySource", "Units"])
+    
+    df = df.reset_index(drop=True)
+    if gen_csv:
+        df.to_csv("cali_powerplant_out.csv", index = False)
+    else:
+        print("Parsed the data")
